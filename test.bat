e039af110e8bf918cbb8da60ac7ef6ebd4c5b718
ShoshinNikita <shoshin.nikita@gmail.com>
2018-06-09 22:06:55 +0300

    Fix test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @ECHO off
 set GOPATH=%cd%
 ECHO Errors:
-C:%HOMEPATH%/go/bin/megacheck bot dates main
-go vet bot dates main
+C:%HOMEPATH%/go/bin/megacheck bot dates main dialogs
+go vet bot dates main dialogs
 ECHO Tests:
 go test ./...
