3e646beac8ac5b74d38d3aa30885aa09d6a944ef
ShoshinNikita <shoshin.nikita@gmail.com>
2018-06-10 12:26:48 +0300

    Improve build.bat

--- build.bat
+++ build.bat
@@ -6,7 +6,9 @@ SET /p command=Should continue(y/n):
 IF "%command%" == "y" (
 	SET GOOS=linux
 	SET GOARCH=amd64
+	ECHO Complie the program
 	go build -o app/radio-t-bot src/main/main.go
 
-	CALL buildImage
+	ECHO Build a docker image
+	docker build -t radio-t-bot .
 )
